d18c81548497f77a6a117490888d4cd69a583ea5
Hugo Roussel <hugo@localhost.localdomain>
2017-10-06 11:36:02 +0200

    Constructor, states of contract, modifier for states, update of README.md

--- README.md
+++ README.md
@@ -1,21 +1,41 @@
 # Proof of Personhood on the Ethereum blockchain
 Concile anonymity and accoutability using proof of Personhood tokens and the Ethereum blockchain
 
-# Setting up the configuration
+## Setting up the configuration of the party
 
-## Step 1 :
+### Step 1, setting up the paramaters :
 
-Organizer 1 deploys the PoP contracts and sends configuration of the party using a private function, registering the different data needed : name, place, date and the Ethereum public addresses of the n other organizers.
+Organizer 1 (admin) deploys the popcontract and sends the configuration of the party using a private function, registering the different data needed : name, place, date and the Ethereum public addresses of the n other organizers. The contract passes in the state "configuration set".
 
-## Step 2 :
+### Step 2, signing the configuration:
 
-Every organizer signs off the config by calling a public function only accessible by the public addresses mentioned earlier. Once, all organizers signed/voted, the contract enters a “set” state.
+Each organizer individually signs off individually the configuration using is public key (the contract matches the address with the initial addresses of the configuration).
+The admin then calls a function to sign the whole configuration. The contract passes in the state "configuration signed".
+NOTE : In the future modify the function such that the other organizers can sign using their private key, thus removing the need of the admin to sign it manually.
 
-Possibility of voting as well on the way to reach consensus later on?
+### Step 3, deposit of public keys of the participants :
 
-Every organizer sends a (sorted) list of public attendees keys to the contract.
-The party ends once the organizers reach a consensus on which keys to push to the blockchain. Multiple ways to determine the set of keys kept :
+Every organizer sends a (sorted) list of public attendees keys to the contract. Until the limit time/date, they are free to update the list.
+Once one set of key has been deposited the contract passes in the state "keyDeposit".
 
-The last deposit with the last set is kept
-Take the set which is sent by the majority of organizers (with or without threshold)
-Wait for consensus (risk of deadlock)
+### Set 4, reach consensus :
+
+The contract decides which set of keys to keep using either :
+
+* The last deposit with the last set is kept
+* Take the set which is sent by the majority of organizers (with or without threshold)
+* Wait for consensus (risk of deadlock)
+
+Once this is done, the contract enters the state "Locked".
+
+## How to deploy using the truffle framework :
+
+http://truffleframework.com/
+
+Run a local version of the Ethereum blockchain using :
+`testrpc`
+
+Then : 
+`truffle compile`
+
+`truffle migrate`
--- contracts/popcontract.sol
+++ contracts/popcontract.sol
@@ -2,68 +2,102 @@ pragma solidity ^0.4.0;
 
 
 contract mortal {
-    /* Define variable owner of the type address */
-    address owner;
+  /* Define variable owner of the type address */
+  address owner;
 
-    /* This function is executed at initialization and sets the owner of the contract */
-    function mortal() { owner = msg.sender; }
+  /* This function is executed at initialization and sets the owner of the contract */
+  function mortal() { owner = msg.sender; }
 
-    /* Function to recover the funds on the contract */
-    function kill() { if (msg.sender == owner) selfdestruct(owner); }
+  /* Function to recover the funds on the contract */
+  function kill() { if (msg.sender == owner) selfdestruct(owner); }
 }
 
 contract popcontract is mortal {
 
-     string nameOfParty;
-     string locationOfParty;
-     uint numberOfOrganizers;
-     address[] organizersAdresses;
-     address[] signedConfiguration;
-     address nullAddress = 0x0000000000000000000000000000000000000000;
 
+  //States of the contract
+   enum contractState {
+     initialState,
+     configurationSet,
+     configurationSigned,
+     keyDeposited,
+     locked
+   }
 
-  function setConfiguration(string name, string place, uint organizers){
+   //can change some of the variables to public to delete getters
+   contractState public currentState;
+   string nameOfParty;
+   string locationOfParty;
+   uint numberOfOrganizers;
+   address[] organizersAdresses;
+   address[] signedConfiguration;
+   address nullAddress = 0x0000000000000000000000000000000000000000;
+
+  modifier onlyState(contractState expectedState){
+    if(expectedState == currentState){_;}
+    else{revert();}
+  }
+
+  //constructor to initialize contract state
+  function popcontract(){
+    currentState = contractState.initialState;
+  }
+
+  //to do : add modifier so only accessible in initialState
+  function setConfiguration (string name, string place, uint organizers, address[] data) onlyState(contractState.initialState) returns (bool) {
+    if(msg.sender == owner){
     nameOfParty = name ;
     locationOfParty = place;
     numberOfOrganizers = organizers;
-
-  }
-
-  function setOrganizersAddresses(address[] data){
     organizersAdresses.length = numberOfOrganizers;
     organizersAdresses = data;
+    currentState = contractState.configurationSet;
+    return true;
+    }else{
+    revert();
+    }
+
   }
 
-    function getName() constant returns (string) {
-        return nameOfParty;
-    }
-    function getPlace() constant returns (string) {
-        return locationOfParty;
-    }
-    function getOrganizersAddresses() constant returns (address[]){
-        return organizersAdresses;
-    }
+  function getName() constant returns (string) {
+      return nameOfParty;
+  }
+  function getPlace() constant returns (string) {
+      return locationOfParty;
+  }
+  function getOrganizersAddresses() constant returns (address[]){
+      return organizersAdresses;
+  }
 
-  //not very elegant but works. To do : remove index
-  //change this functions such that it returns the organizer index instead of the address registered
-  function signConfig() returns (address){
-
-    //verify if msg.sender address is in organizersAdresses
-    bool pres = false;
-    uint i=0;
-    for (i; i< numberOfOrganizers; i++){
+  function configSignOrganizers() onlyState(contractState.configurationSet) returns (bool){
+    //verify if msg.sender address is in organizersAdresses and sign if it is case
+    for (uint i; i< numberOfOrganizers; i++){
       if(msg.sender == organizersAdresses[i]){
-        pres = true ;
+        signedConfiguration[i] = msg.sender;
+        return true;
       }
     }
+    return false;
+  }
 
-    //add the address only if valid organizer.
-    if(pres){
-      signedConfiguration[i] = msg.sender;
-      return msg.sender;
-    } else{
-      return nullAddress;
+  //after the signing of the configuration by the organizers the admin changes the state of the contract
+  function signWholeConfiguration() onlyState(contractState.configurationSet) returns (bool){
+    bool correct = false;
+    if(msg.sender == owner){
+      for(uint i =0; i< numberOfOrganizers; i++){
+        if(signedConfiguration[i]==nullAddress){
+          revert();
+        } else{
+          correct = true;
+        }
+      }
+      if(correct){
+        currentState = contractState.configurationSigned;
+        return true;
+      }
     }
   }
+
+
 
 }
